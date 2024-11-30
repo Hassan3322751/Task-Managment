1e29c16cb0ded8ca3e2e3e6d7004bf5409f21127
Hassan3322751 <mianhassan155156@gmail.com>
2024-11-30 20:14:26 +0500

    app.js updated

--- Server/app.js
+++ Server/app.js
@@ -31,11 +31,6 @@ app.use("/api/projects", projectsRoutes);
 app.use("/api/stages", stageRoutes);
 app.use("/api/tasks", tasksRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
-}
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Backend is running on port ${port}`);
