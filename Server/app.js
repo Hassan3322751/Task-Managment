a6624895353d09a38e1a2bf72493d8695442080a
Hassan3322751 <mianhassan155156@gmail.com>
2024-11-30 20:02:12 +0500

    cors added

--- Server/app.js
+++ Server/app.js
@@ -11,7 +11,7 @@ const tasksRoutes = require("./routes/tasksRoutes");
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://task-managment-og13.vercel.app'],
+  origin: ['http://localhost:5173', 'https://task-managment-ten-kappa.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
